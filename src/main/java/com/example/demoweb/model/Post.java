8dce8c9cac8f6aa5cb6b5909ee5cac8e66b44c93
Tyan v sharfe <thisonlystudy@gmail.com>
2023-02-12 15:30:32 +0500

    V2.6

--- src/main/java/com/example/demoweb/model/Post.java
+++ src/main/java/com/example/demoweb/model/Post.java
@@ -1,13 +1,17 @@
 package com.example.demoweb.model;
 
+import java.util.Date;
+
 public class Post {
     private String text;
     private Integer likes;
+    private Date creationDate;
 
-    public Post(String text)
+    public Post(String text, Date date)
     {
         this.text = text;
         this.likes = 0;
+        this.creationDate = date;
     }
 
     public String getText() {
@@ -16,5 +20,9 @@ public class Post {
 
     public Integer getLikes() {
         return likes;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
     }
 }
--- src/main/java/com/example/demoweb/service/PostService.java
+++ src/main/java/com/example/demoweb/service/PostService.java
@@ -4,15 +4,16 @@ import com.example.demoweb.model.Post;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 @Service
 public class PostService {
 
     public List<Post> listAllPosts() {
-        Post post1 = new Post("Оригинальную Fallout теперь можно установить на смартфон");
-        Post post2 = new Post("Microsoft подтвердила сбой приложений Windows 11 из-за драйверов Intel");
-        Post post3 = new Post("Apple ограничит использование порта USB-C в iPhone 15");
+        Post post1 = new Post("Оригинальную Fallout теперь можно установить на смартфон", new Date());
+        Post post2 = new Post("Microsoft подтвердила сбой приложений Windows 11 из-за драйверов Intel", new Date());
+        Post post3 = new Post("Apple ограничит использование порта USB-C в iPhone 15", new Date());
 
         return Arrays.asList(post1,post2,post3);
     }
