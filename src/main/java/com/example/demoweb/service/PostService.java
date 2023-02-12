f86bf5cb9505226288e85f44b66ff016b5fb52db
Tyan v sharfe <thisonlystudy@gmail.com>
2023-02-12 23:57:12 +0500

    V2.65

--- src/main/java/com/example/demoweb/service/PostService.java
+++ src/main/java/com/example/demoweb/service/PostService.java
@@ -3,18 +3,23 @@ package com.example.demoweb.service;
 import com.example.demoweb.model.Post;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
 @Service
 public class PostService {
+    private ArrayList<Post> posts = new ArrayList<>(Arrays.asList(
+            new Post("Оригинальную Fallout теперь можно установить на смартфон", new Date()),
+            new Post("Microsoft подтвердила сбой приложений Windows 11 из-за драйверов Intel", new Date()),
+            new Post("Apple ограничит использование порта USB-C в iPhone 15", new Date())));
 
     public List<Post> listAllPosts() {
-        Post post1 = new Post("Оригинальную Fallout теперь можно установить на смартфон", new Date());
-        Post post2 = new Post("Microsoft подтвердила сбой приложений Windows 11 из-за драйверов Intel", new Date());
-        Post post3 = new Post("Apple ограничит использование порта USB-C в iPhone 15", new Date());
+        return posts;
+    }
 
-        return Arrays.asList(post1,post2,post3);
+    public void create(String text) {
+        posts.add(new Post(text, new Date()));
     }
 }
